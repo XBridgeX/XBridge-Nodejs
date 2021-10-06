6d57ef60f99c8af04a59bc9ff320f474a01a34da
yanhy <2805214723@qq.com>
2021-10-06 20:57:00 +0800

    update

---
+++ Utils/PackHelper.js
@@ -0,0 +1,56 @@
+const AES = require("./AES.js");
+
+function guid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+/** 
+* 执行命令数据包
+* @param k 加密密匙
+* @param iv 加密偏移量
+* @param cmd 要执行的命令
+* @param id 执行附带的ID，返回包时会附带
+*/
+function GetRuncmdPack(k,iv,cmd,id){
+    var p = {
+        type : "pack",
+        action : "runcmdrequest",
+        params : {
+            cmd : cmd,
+            id : id
+        }
+    }
+    return GetEncrypt(k,iv,JSON.stringify(p))
+}
+/** 
+* 发送文本数据包
+* @param k 加密密匙
+* @param iv 加密偏移量
+* @param text 发送到服务端的文本
+*/
+function GetSendTextPack(k,iv,text){
+    var p = {
+        type : "pack",
+        action : "sendtext",
+        params : {
+            text : text,
+            id : guid()
+        }
+    }
+    return GetEncrypt(k,iv,JSON.stringify(p))
+}
+
+function GetEncrypt(k,iv,pack){
+    var p = {
+        type : "encrypt",
+        params = {
+            mode : "aes_cbc_pck7padding",
+            raw = AES.encrypt(k,iv,pack)
+        }
+    };
+    return JSON.stringify(p);
+}
--- app.js
+++ app.js
@@ -1,18 +1,57 @@
+"use strict"
+//启用nodejs严格模式
+// const { bot } = require("../../index");//尚未启用机器人，调试的时候会报错
 const ws = require("./Utils/Websocket");
 const AES = require("./Utils/AES");
 const MD5 = require("./Utils/MD5");
+var fs = require('fs');
 
-var client = ws.GetWebsocketClient("ws://127.0.0.1:8080","生存服务器","password");
+//bot示例
+// bot.on("message.group", function (e) {
+//     if(e.raw_message == "test"){
+//         e.reply("11132测试！");
+//     }
+// })
+
+let config = JSON.parse(fs.readFileSync("data/config.json"));
+const address = config.ws_address;
+const servername = config.server_name;
+const password = config.password;
+var client = ws.GetWebsocketClient(address , servername , password);
+
 client.ws.on("connect",function(con){
+    console.log("ws connect!")
     con.on("message",function(m){
+        
         try
         {
-            //do someting here..
+            //doing...
+            var TempData = AES.decrypt(client.k,client.iv,JSON.parse(m.utf8Data).params.raw);
             console.log(AES.decrypt(client.k,client.iv,JSON.parse(m.utf8Data).params.raw));
+            let data = JSON.parse(TempData)
+            let cause = data.cause;
+            let params = data.params;
+            console.log(data,cause,params)
+            //to do...
+            // switch(cause)
+            // {
+            //     case "join":{};break;
+            // }
+
         }catch(err)
         {
-            console.log(err.message);
+            console.log("err=",err.message);
         }
     })
+    con.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    con.on('close', function() {
+        console.log('Connection Closed');
+    });
 });
+client.ws.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
 client.Connect();
