eb1a155ef1a9f8e0d5fe591b5e3ac7d215366a01
Asurin <77911856+Asurin219@users.noreply.github.com>
2021-10-27 20:02:00 +0800

    Updates (#12)
    * 新增示例配置
    
    * 忽略config目录
    
    * 更新内容
    
    * 新增Linux运行脚本
    
    * 新增大量功能
    
    * 更换配置文件目录、其他更新

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var conf = require("./main");
 const ws = require("./Utils/Websocket");
 const AES = require("./Utils/AES");
 const MD5 = require("./Utils/MD5");
+const http = require('http');
 const PHelper = require("./Utils/PackHelper")
 var fs = require('fs');
 var ws_send = null //初始化全局websocket对象
@@ -14,84 +15,392 @@ var servername = conf.servername;
 var passwd = conf.ws_passwd;
 var groupID = conf.groupID;
 
+var client = ws.GetWebsocketClient(address , servername , passwd);
+
+let cfg_path = conf.datapath
+let regex_json = cfg_path+"/regex.json"
+let mobs_json = cfg_path+"/mobs.json"
+let players_info_json = cfg_path+"/players_info.json"
+
 var k = "1234567890123456";
 var iv = "1234567890123456";
 k = MD5.MD5(passwd).toUpperCase().substring(0,16);
 iv = MD5.MD5(passwd).toUpperCase().substring(16,32);
-//bot示例
-bot.on("message.group", function (e) {
-    if(e.raw_message == "test"){
-        e.reply("11132测试！");
-	ws_send.sendUTF(PHelper.GetRuncmdPack(k,iv,"stop",0));  //主动发信示例
+
+
+//控制台消息
+function logger(e)
+{
+	console.log("[XBridge] "+e)
+};
+
+//配置文件初始化
+function prepare(){
+    if(!fs.existsSync(cfg_path)){   //创建config文件夹
+        fs.mkdirSync(cfg_path)
+    }
+
+    try{        //正则配置初始化
+        if(fs.openSync(regex_json,'r')){
+			logger("正在加载正则配置...") 
+		}
+    }catch(err){
+        logger("正则配置不存在，使用示例配置进行创建...");
+        try{
+            fs.copyFileSync("./example/regex_example.json",regex_json);
+            logger("正则配置创建成功！")
+        }
+        catch(err){
+            logger("正则配置创建失败：示例配置不存在，请到XBridge交流群寻求技术支持！")
+        }
+    }
+
+    try{        //玩家数据初始化
+        if(fs.openSync(players_info_json,'r')){
+			logger("正在加载玩家数据...") 
+		}
+    }catch(err){
+        logger("玩家数据不存在，正在初始化...")
+        var file_open = fs.openSync(players_info_json,'w');
+        fs.writeSync(file_open, "[]");
+        fs.closeSync(file_open)
+        logger("玩家数据初始化完成！")
+    }
+
+    try{        //实体数据初始化
+        if(fs.openSync(mobs_json,'r')){
+			logger("正在加载实体数据...") 
+		}
+    }catch(err){
+        logger("实体数据不存在，使用示例配置进行创建...");
+        try{
+            fs.copyFileSync("./example/mobs_example.json",mobs_json);
+            logger("实体数据创建成功！")
+        }
+        catch(err){
+            logger("实体数据创建失败：示例配置不存在，请到XBridge交流群寻求技术支持！")
+        }
+    }
+};
+prepare();
+
+
+
+bot.on("message.group", function(e){    //开始监听群消息
+    if(e.group_id == groupID){
+        logic_main(e)
     }
 })
 
-function logger(e)
-{
-	console.log("[XB]"+e)
-};
+//公共方法1：玩家权限判断
+function permission(e) {
+    let players_info = JSON.parse(fs.readFileSync(players_info_json))
+    for(var i=0 ; i<players_info.length ; i++){
+        if(players_info[i].qqid == e.user_id){
+            let permission = players_info[i].permission
+            switch(permission){
+                case 0:{
+                    return 0
+                }
+                case 1:{
+                    return 1
+                }
+            }
+        }
+    }
+}
 
-logger("准备加载XBridge...");
+//公共方法2：玩家数据读写
+function file_rw(e,players_info_tmp,content){
+    let players_info = JSON.stringify(players_info_tmp,null,'\t');
+    let open_players_info = fs.openSync(players_info_json,'w');
+    fs.writeSync(open_players_info, players_info);
+    fs.closeSync(open_players_info);
+    e.reply("[CQ:at,qq="+e.user_id+"]\n"+content);
+}
 
-var client = ws.GetWebsocketClient(address , servername , passwd);
+//主逻辑
+function logic_main(e){
+    let reg = JSON.parse(fs.readFileSync(regex_json))
+    for (var a=0 ; a<reg.length ; a++){
+        let re = new RegExp(reg[a].keywords,"g");
+        if(e.raw_message == e.raw_message.match(re)){
+            let r = (reg.indexOf(reg.filter(d=>d.keywords===d.keywords)[a]))
+            if(reg[r].permission == 1 && permission(e) != 1){     //如果正则要求权限为1，但玩家非管理员，则告知无权，跳出循环
+                e.reply("[CQ:at,qq="+e.user_id+"]\n您不是管理员，无权执行该操作！");
+                continue
+            }
+            else{
+                for (var b=0 ; b<reg[r].actions.length ; b++) {    //遍历动作列表
+                    let type = reg[r].actions[b].type;
+                    let content = reg[r].actions[b].content;
+                    let succeed = reg[r].actions[b].succeed;
+                    let failed = reg[r].actions[b].failed;
+                    modules(e,re,type,content,succeed,failed)
+                }
+            }
+        }
+    }
+}   
 
-client.ws.on("connect",function(con){
-    console.log("WS服务器连接成功！")
+//功能模块（白名单加减、自定义命令等）
+function modules(e,re,type,content,succeed,failed){
+    switch(type)
+    {
+        case "runcmd":{     //执行命令
+            ws_send.sendUTF(PHelper.GetRuncmdPack(k,iv,content))
+        };
+        break;
+
+        case "group_message":{  //发送群消息
+            e.reply("[CQ:at,qq="+e.user_id+"]\n"+content);
+        };
+        break;
+
+        case "bind_whitelist":{    //自助绑定白名单（玩家）
+            let xboxid = e.raw_message.replace(re,"$1");
+            let players_info_tmp = JSON.parse(fs.readFileSync(players_info_json));
+            if(players_info_tmp.some( (val) => val.name===xboxid)){
+                e.reply("[CQ:at,qq="+e.user_id+"]\n白名单申请已受理，请勿重复提交！");
+            }
+            else{
+                let add_xboxid = {"name":xboxid,"qqid":e.user_id,"permission":0,"enable":false};
+                players_info_tmp.push(add_xboxid);
+                return file_rw(e,players_info_tmp,content)
+            }
+        };
+        break;
+
+        case "add_whitelist":{      //加白名单(管理员)
+            for (var f=0 ; f<e.message.length ; f++){
+                let at_qqid = e.message[f].data.qq
+                let players_info_tmp = JSON.parse(fs.readFileSync(players_info_json));
+                if(e.message[f].type == "at" ){
+                    if(players_info_tmp.some( (val) => val.qqid===at_qqid)){
+                        let s = (players_info_tmp.indexOf(players_info_tmp.filter(d=>d.qqid===at_qqid)[0]))
+                        if (players_info_tmp[s].enable){
+                            e.reply("[CQ:at,qq="+e.user_id+"]\n该玩家已经绑定，且已添加过白名单过了！")
+                        }
+                        else{
+                            ws_send.sendUTF(PHelper.GetRuncmdPack(k,iv,"whitelist add \""+players_info_tmp[s].name+"\""))
+                            players_info_tmp[s].enable = true;
+                            return file_rw(e,players_info_tmp,content)
+                        }
+                    }
+                    else{
+                        e.reply("[CQ:at,qq="+e.user_id+"]\n该玩家未绑定，无法为其添加白名单！");
+                    }
+                }
+            }
+        };
+        break;
+
+        case "bind_check_self":{   //个人自查
+            let players_info_tmp = JSON.parse(fs.readFileSync(players_info_json));
+            if(players_info_tmp.some((val) => val.qqid===e.user_id)){
+                
+                var s = players_info_tmp.indexOf(players_info_tmp.filter(d=>d.qqid === e.user_id)[0]);
+                var reply_xboxid = players_info_tmp[s].name
+                var reply_qqid = players_info_tmp[s].qqid
+
+                switch(players_info_tmp[s].permission){
+                    case 0:
+                        var reply_permission = "普通成员"
+                    break;
+
+                    case 1:
+                        var reply_permission = "管理员"
+                    break;
+                }
+
+                switch(players_info_tmp[s].enable){
+                    case true:
+                        var reply_wl_status = "已添加"
+                    break;
+
+                    case false:
+                        var reply_wl_status = "待添加"
+                    break;
+                }
+                e.reply("[CQ:at,qq="+e.user_id+"] "+content+"\nXbox ID："+reply_xboxid+"\nQQ："+reply_qqid+"\n权限等级："+reply_permission+"\n白名单状态："+reply_wl_status);
+            }
+            else{
+                e.reply("[CQ:at,qq="+e.user_id+"]\n你还没有绑定！")
+            }
+        };
+        break
+
+        case "bind_check":{
+            for (var j=0 ; j<e.message.length ; j++){
+                let at_qqid = e.message[j].data.qq
+                let players_info_tmp = JSON.parse(fs.readFileSync(players_info_json));
+                if(e.message[j].type == "at" ){
+                    if(players_info_tmp.some( (val) => val.qqid===at_qqid)){
+                        let s = (players_info_tmp.indexOf(players_info_tmp.filter(d=>d.qqid===at_qqid)[0]))
+                        var reply_xboxid = players_info_tmp[s].name
+                        var reply_qqid = players_info_tmp[s].qqid
+
+                        switch(players_info_tmp[s].permission){
+                            case 0:
+                                var reply_permission = "普通成员"
+                            break;
+
+                            case 1:
+                                var reply_permission = "管理员"
+                            break;
+                        }
+
+                        switch(players_info_tmp[s].enable){
+                            case true:
+                                var reply_wl_status = "已添加"
+                            break;
+
+                            case false:
+                                var reply_wl_status = "待添加"
+                            break;
+                        }
+                        e.reply("[CQ:at,qq="+e.user_id+"] "+content+"\nXbox ID："+reply_xboxid+"\nQQ："+reply_qqid+"\n权限等级："+reply_permission+"\n白名单状态："+reply_wl_status);
+                    }
+                    else{
+                        e.reply("[CQ:at,qq="+e.user_id+"]\n未查询到该玩家的信息！");
+                    }
+                }
+            }
+        };
+        break;
+
+        case "unbind_whitelist":{   //解绑（玩家）
+            let players_info_tmp = JSON.parse(fs.readFileSync(players_info_json));
+            if(players_info_tmp.some((val) => val.qqid===e.user_id)){
+                let s = players_info_tmp.indexOf(players_info_tmp.filter(d=>d.qqid === e.user_id)[0]);
+                ws_send.sendUTF(PHelper.GetRuncmdPack(k,iv,"whitelist remove \""+players_info_tmp[s].name+"\""))
+                players_info_tmp.splice(s,1);
+                return file_rw(e,players_info_tmp,content)
+            }
+            else{
+                e.reply("[CQ:at,qq="+e.user_id+"]\n你还没有绑定！");
+            }
+        };
+        break;
+
+        case "del_whitelist":{      //删白名单(管理员)
+            for (var f=0 ; f<e.message.length ; f++){
+                let at_qqid = e.message[f].data.qq
+                let players_info_tmp = JSON.parse(fs.readFileSync(players_info_json));
+                if(e.message[f].type == "at" ){
+                    if(players_info_tmp.some( (val) => val.qqid===at_qqid)){
+                        let s = players_info_tmp.indexOf(players_info_tmp.filter(d=>d.qqid === at_qqid)[0]);
+                        ws_send.sendUTF(PHelper.GetRuncmdPack(k,iv,"whitelist remove \""+players_info_tmp[s].name+"\""));
+                        players_info_tmp.splice(s,1);
+                        return file_rw(e,players_info_tmp,content)
+                    }
+                    else{
+                        e.reply("[CQ:at,qq="+e.user_id+"]\n该玩家未绑定！");
+                    }
+                }
+            }
+
+        };break;
+
+        case "http_get":{   //发起http_get请求
+            http.get(content,(res) => {
+                //console.log(`${res.statusCode}OK`);
+                e.reply(succeed)
+                res.resume();
+
+                }).on('error', (err) => {
+                    e.reply(failed)
+                    console.log(`${err.message}`);
+                })
+        };
+        break
+    }
+}
+
+
+
+client.Connect();                       //建立WS客户端连接
+client.ws.on("connect",function(con){   //WS客户端连接成功
+    logger("WS服务器连接成功！")
     ws_send = con //装载全局ws对象
+    let mobs = JSON.parse(fs.readFileSync(mobs_json))  //实体数据
     con.on("message",function(m){   
         try
         {
             var TempData = AES.decrypt(client.k,client.iv,JSON.parse(m.utf8Data).params.raw);
-            let data = JSON.parse(TempData)
+            let data = JSON.parse(TempData);
             let cause = data.cause;
             let e = data.params;
-            console.log(data,cause,e)
+
             switch(cause)
             {
                 case "join":{
                     let player = e.sender;
-                    bot.sendGroupMsg(groupID[0], "玩家 "+player+" 进入了服务器！");
+                    bot.sendGroupMsg(groupID[0], "玩家 "+player+" 杀进了服务器");
                 };break;
                 case "left":{
                     let player = e.sender;
-                    bot.sendGroupMsg(groupID[0], "玩家 "+player+" 离开了服务器！");
+                    bot.sendGroupMsg(groupID[0], "玩家 "+player+" 爬出了服务器");
+                };break;
+                case "mobdie":{     //玩家死亡事件
+                    if (e.mobname == e.mobtype)
+                    {
+                        let srctype = e.srctype
+                        let diepl = e.mobname
+
+                        for (var key in mobs){
+                            if(srctype == key){
+                                let str = diepl+"被"+mobs[srctype]+"干趴了";
+                                bot.sendGroupMsg(groupID[0],str);
+                                break;
+                            }
+                        };
+                        if(srctype == "unknown"){
+                            bot.sendGroupMsg(groupID[0],diepl+"啪唧一下死掉了（不知道怎么死的）")
+                        }
+                    }
                 };break;
                 case "chat":{
                     let player = e.sender;
                     let chat = e.text;
-                    bot.sendGroupMsg(groupID[0], "[服聊] "+player+" >> "+chat);
+                    bot.sendGroupMsg(groupID[0], "["+servername+"] <"+player+">\n "+chat);
                 };break;
-                case "runcmdfeedback":{};break;
+                case "runcmdfeedback":{
+                    bot.sendGroupMsg(groupID[0],e.result)
+                };break;
                 case "start":{
-                    let str = "服务器启动！";
+                    let str = "服务器已启动";
                     console.log(str);
                     bot.sendGroupMsg(groupID[0], str);
                 };break;
                 case "stop":{
-                    let str = "服务器关闭！"
+                    let str = "服务器已关闭"
                     console.log(str);
                     bot.sendGroupMsg(groupID[0], str);
                 };break;
                 case "plantext":{};break;
                 case "decodefailed":{
-                    bot.sendGroupMsg(groupID[0], "数据包解析异常，请前往后台查看");
-                    console.log("数据包解析异常：",e.msg)
+                    bot.sendGroupMsg(groupID[0], "数据包解析失败，请前往后台查看");
+                    logger("数据包解析失败：",e.msg)
                 };break;
             }
         }catch(err)
         {
-            console.log("异常信息：",err.message);
+            console.log(motd+"异常信息：",err.message);
         }
     })
     con.on('error', function(error) {
-        console.log("WS连接出错: " + error.toString());
+        logger("WS连接出错: " + error.toString());
     });
     con.on('close', function() {
-        console.log('WS连接已关闭！');
+        logger("WS连接已关闭！");
+        bot.sendGroupMsg(groupID[0],"服务器已关闭")
+        setTimeout(function(){client.Connect()},5000)
     });
 });
-client.ws.on('connectFailed', function(error) {
-    console.log('WS连接失败: ' + error.toString());
+
+client.ws.on('connectFailed', function(error) {     //WS客户端连接失败
+    logger("WS连接失败: " + error.toString());
+    setTimeout(function(){client.Connect()},5000)
    
 });
-client.Connect();
--- main.js
+++ main.js
@@ -1,119 +1,112 @@
 "use strict"
 var fs = require('fs');
-let path = require('path');
-let readline = require('readline')
-let data = "./data/config.json"
-let datapath = "./data"
+//let path = require('path');
+let data = "./config/global_setting.json"
+let datapath = "./config"
+let ver = "1.0.0_beta10262108"
 
-function logger(e)
-{
+function logger(e){
 	console.log(e)
 };
 
-setTimeout(()=>logger('欢迎使用XBridge&OCL集成版'),100);
-
 function init(){
-	if(!fs.existsSync(datapath)){fs.mkdirSync(datapath)}
-	try{
-		if(fs.openSync(data,'r'))//配置文件创建
-		{
-			logger("[INFO]检测配置文件存在，准备启动BOT...") 
-		}}catch(err){
-			logger("[INFO]配置文件不存在！准备自动创建...")
-			let jsonData = {
-				"qq": 123456,
-				"login_qrcode":true,
-				"qq_password":"qqpassword",
-                "ws_address": "ws://127.0.0.1:8080",
-                "server_name": "生存服务器",
-                "ws_password": "wspassword",
-                "qq_group":[123456]
-			}
-			let text = JSON.stringify(jsonData,null,'\t');
-			let file = path.join(datapath, 'config.json');
-			var fd = fs.openSync(data,'w');
-			fs.writeSync(fd, text);
-			setTimeout(()=>logger('[INFO]文件创建成功！文件名：' + file+"\n-------------------------\n[WARN]第一次文件创建完成请手动修改配置文件后使用！3s后准备强制退出...\n-------------------------"),100);
-			fs.closeSync(fd);
-			setTimeout(function(){process.exit(0)},3000)//强制退出，延时3s
+    logger("\n                                                       ████████████\n ██╗  ██╗██████╗ ██████╗ ██╗██████╗  ██████╗ ███████╗  ██   ███  ██\n ╚██╗██╔╝██╔══██╗██╔══██╗██║██╔══██╗██╔════╝ ██╔════╝  ██    ██  ██\n  ╚███╔╝ ██████╔╝██████╔╝██║██║  ██║██║  ███╗█████╗    ██  █  █  ██\n  ██╔██╗ ██╔══██╗██╔══██╗██║██║  ██║██║   ██║██╔══╝    ██  ██    ██\n ██╔╝ ██╗██████╔╝██║  ██║██║██████╔╝╚██████╔╝███████╗  ██  ███   ██\n ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝╚═════╝  ╚═════╝ ╚══════╝  ████████████\n                      - Powered by OICQ -\n\n[INFO] XBridge-N版本："+ver);
+	if(!fs.existsSync(datapath)){   //创建配置文件目录
+        fs.mkdirSync(datapath)
+    }
+	try{                            //全局配置检测
+		if(fs.openSync(data,'r')){      //配置文件存在
+			logger("[INFO] 正在加载全局配置...")
+            ocl_core()      //开始加载ocl核心
 		}
+    }
+    catch(err){                     //配置文件不存在
+        logger("[INFO] 全局配置不存在，正在自动创建...")
+        let jsonData = {
+            "qq": "",
+            "login_qrcode":true,
+            "qq_password":"",
+            "ws_address": "",
+            "server_name": "",
+            "ws_password": "",
+            "qq_group":[""]
+        }
+        
+        let text = JSON.stringify(jsonData,null,'\t');
+        var fd = fs.openSync(data,'w');
+        fs.writeSync(fd, text);
+        fs.closeSync(fd);
+        console.log("[INFO] 全局配置创建成功！请先修改配置文件，再启动XBridge-N\n")
+    }
 }
-
 init();
-console.log(" ██     ██ ██████          ██      ██                \n░░██   ██ ░█░░░░██        ░░      ░██  █████         \n ░░██ ██  ░█   ░██  ██████ ██     ░██ ██░░░██  █████ \n  ░░███   ░██████  ░░██░░█░██  ██████░██  ░██ ██░░░██\n   ██░██  ░█░░░░ ██ ░██ ░ ░██ ██░░░██░░██████░███████\n  ██ ░░██ ░█    ░██ ░██   ░██░██  ░██ ░░░░░██░██░░░░ \n ██   ░░██░███████ ░███   ░██░░██████  █████ ░░██████\n░░     ░░ ░░░░░░░  ░░░    ░░  ░░░░░░  ░░░░░   ░░░░░░ \n");
-let config = JSON.parse(fs.readFileSync(data));
-const loginway = config.login_qrcode;//登录方式，默认为为true；（true：扫码登录，false：密码登录）
-const account = config.qq;//机器人qq号码
-const qq_passwd = config.qq_password;//机器人qq密码
-const address = config.ws_address;//ws地址
-const servername = config.server_name;//服务端名称
-const ws_passwd = config.ws_password;//ws密钥
-const groupID = config.qq_group;//群号组
 
-const conf = {//机器人内部配置
-		platform: 2,//QQ登录协议。1:安卓手机 2:安卓平板 3:安卓手表 4:MacOS 5:iPad
-		kickoff: false,
-		ignore_self: true,
-		resend: true,
-		brief: true		
-}
-const bot = require("oicq").createClient(account,conf)
-setTimeout(()=>{//登录部分
-//默认扫码登录
-if(loginway)
-{
-	bot.on("system.login.qrcode", function (e) {
-		this.logger.mark("扫码后按Enter完成登录") 
-		process.stdin.once("data", () => {
-			this.login()
-		})
-	})
-	.on("system.login.error", function (e) {
-		if (e.code < 0)
-			this.login()
-	})
-	.login()
-}
-//密码登录
-else{
-	bot.on("system.login.slider", function (event) {
-		this.logger.mark("需要验证滑块登录！") 
-		process.stdin.once("data", (input) => {
-		  this.sliderLogin(input);
-		});
-	  }).on("system.login.device", function (event) {
-		this.logger.mark("验证完成后按回车登录") 
-		process.stdin.once("data", () => {
-		  this.login();
-		});
-	  }).login(qq_passwd);
-}
-},3500)
+function ocl_core(){
+    let config = JSON.parse(fs.readFileSync(data));
+    const loginway = config.login_qrcode;//登录方式，默认为为true；（true：扫码登录，false：密码登录）
+    const account = config.qq;//机器人qq号码
+    const qq_passwd = config.qq_password;//机器人qq密码
+    const address = config.ws_address;//ws地址
+    const servername = config.server_name;//服务端名称
+    const ws_passwd = config.ws_password;//ws密钥
+    const groupID = config.qq_group;//群号组
 
-setTimeout(function link(){
+    const conf = {//机器人内部配置
+        platform: 2,//QQ登录协议。1:安卓手机 2:安卓平板 3:安卓手表 4:MacOS 5:iPad
+        kickoff: false,
+        ignore_self: true,
+        resend: true,
+        brief: true		
+    }
+    const bot = require("oicq").createClient(account,conf)
+    setTimeout(()=>{//登录部分
+    //默认扫码登录
+    if(loginway)
+    {
+    bot.on("system.login.qrcode", function (e) {
+        this.logger.mark("扫码后按Enter完成登录") 
+        process.stdin.once("data", () => {
+            this.login()
+        })
+    })
+    .on("system.login.error", function (e) {
+        if (e.code < 0)
+            this.login()
+    })
+    .login()
+    }
+    //密码登录
+    else{
+    bot.on("system.login.slider", function (event) {
+        this.logger.mark("需要验证滑块登录！") 
+        process.stdin.once("data", (input) => {
+            this.sliderLogin(input);
+        });
+        }).on("system.login.device", function (event) {
+        this.logger.mark("验证完成后按回车登录") 
+        process.stdin.once("data", () => {
+            this.login();
+        });
+        }).login(qq_passwd);
+    }
+    },3500)
+
+    setTimeout(function link(){
     exports.bot = bot;//主程序;
     exports.address = address;
     exports.servername = servername;
-	exports.qq_passwd = qq_passwd;
+    exports.qq_passwd = qq_passwd;
     exports.ws_passwd = ws_passwd;
     exports.groupID = groupID;
-	require("./app");
-},3000);
+    exports.datapath = datapath
+    require("./app");
+    },3000);
 
-//控制台指令控制
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-rl.on('line', (str) => {
-    if (str === 'stop') {
-		logger("Bot即将退出...")
-		setTimeout(function(){process.exit(0)},1000)
-    }
-})
-// rl.on('line', (str) => {
-//     if (str === 'reload') {
-// 		logger("插件正在重载...")
-// 		setTimeout(function(){load()},1000)
-//     }
-// })
+    process.stdin.on("data", (input)=>{
+        let i = input.toString().trim();
+        if (i == "stop"){
+            logger("XBridge-N即将退出...")
+            setTimeout(function(){process.exit(0)},1000)
+        }
+    })
+}
