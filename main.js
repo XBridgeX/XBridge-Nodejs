9bc6a76f8ee634127cb8dd959208ab0b99dc4053
Asurin219 <824907403@qq.com>
2021-10-29 10:45:32 +0800

    一些更新 ignore ./config 更正描述 更换配置文件目录、logo

--- main.js
+++ main.js
@@ -2,15 +2,13 @@
 var fs = require('fs');
 let path = require('path');
 let readline = require('readline')
-let data = "./data/config.json"
-let datapath = "./data"
+let data = "./config/global_setting.json"
+let datapath = "./config"
 
 function logger(e)
 {
 	console.log(e)
 };
-
-setTimeout(()=>logger('欢迎使用XBridge&OCL集成版'),100);
 
 function init(){
 	if(!fs.existsSync(datapath)){fs.mkdirSync(datapath)}
@@ -30,7 +28,7 @@ function init(){
                 "qq_group":[123456]
 			}
 			let text = JSON.stringify(jsonData,null,'\t');
-			let file = path.join(datapath, 'config.json');
+			let file = path.join(datapath, 'global_setting.json');
 			var fd = fs.openSync(data,'w');
 			fs.writeSync(fd, text);
 			setTimeout(()=>logger('[INFO]文件创建成功！文件名：' + file+"\n-------------------------\n[WARN]第一次文件创建完成请手动修改配置文件后使用！3s后准备强制退出...\n-------------------------"),100);
@@ -40,7 +38,7 @@ function init(){
 }
 
 init();
-console.log(" ██     ██ ██████          ██      ██                \n░░██   ██ ░█░░░░██        ░░      ░██  █████         \n ░░██ ██  ░█   ░██  ██████ ██     ░██ ██░░░██  █████ \n  ░░███   ░██████  ░░██░░█░██  ██████░██  ░██ ██░░░██\n   ██░██  ░█░░░░ ██ ░██ ░ ░██ ██░░░██░░██████░███████\n  ██ ░░██ ░█    ░██ ░██   ░██░██  ░██ ░░░░░██░██░░░░ \n ██   ░░██░███████ ░███   ░██░░██████  █████ ░░██████\n░░     ░░ ░░░░░░░  ░░░    ░░  ░░░░░░  ░░░░░   ░░░░░░ \n");
+console.log("\n                                                       ████████████\n ██╗  ██╗██████╗ ██████╗ ██╗██████╗  ██████╗ ███████╗  ██   ███  ██\n ╚██╗██╔╝██╔══██╗██╔══██╗██║██╔══██╗██╔════╝ ██╔════╝  ██    ██  ██\n  ╚███╔╝ ██████╔╝██████╔╝██║██║  ██║██║  ███╗█████╗    ██  █  █  ██\n  ██╔██╗ ██╔══██╗██╔══██╗██║██║  ██║██║   ██║██╔══╝    ██  ██    ██\n ██╔╝ ██╗██████╔╝██║  ██║██║██████╔╝╚██████╔╝███████╗  ██  ███   ██\n ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝╚═════╝  ╚═════╝ ╚══════╝  ████████████\n                      - Powered by OICQ -\n\n");
 let config = JSON.parse(fs.readFileSync(data));
 const loginway = config.login_qrcode;//登录方式，默认为为true；（true：扫码登录，false：密码登录）
 const account = config.qq;//机器人qq号码
