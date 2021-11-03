7637a8eb82eacac824d4632a9649f5639bf7673b
Asurin219 <824907403@qq.com>
2021-11-03 23:51:18 +0800

    更新版本号

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 //let path = require('path');
 let data = "./config/global_setting.json"
 let datapath = "./config"
-let ver = "1.0.0_beta11012343"
+let ver = "1.0.0_beta11031646"
 
 function logger(e){
 	console.log(e)
